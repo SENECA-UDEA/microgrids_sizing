7e2116098ec6a179c418fb4f7966d65b6febb882
SebastianCastellanosUdeA <sebastian.castellanos@udea.edu.co>
2022-05-03 22:47:26 -0500

    Comment all, delete constraints and unnecessay process

--- src/classes.py
+++ src/classes.py
@@ -5,102 +5,102 @@ Created on Wed Apr 20 11:04:49 2022
 @author: pmayaduque
 """
 
-class Generator():
+class Generator(): #Superclass generators
 
-    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, c_min, c_max):
-        self.id_gen = id_gen
-        self.tec = tec
-        self.br = br
-        self.va_op = va_op
-        self.area = area
-        self.cost_up = cost_up
-        self.cost_r = cost_r
-        self.cost_om = cost_om
-        self.cost_s = cost_s
-        self.c_min = c_min
-        self.c_max = c_max
+    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s):
+        self.id_gen = id_gen #id of the generator
+        self.tec = tec #technology associated to the generator
+        self.br = br #brand of the generator
+        self.va_op = va_op #Operative cost 
+        self.area = area #Generator area
+        self.cost_up = cost_up #Investment cost
+        self.cost_r = cost_r #Replacement cost
+        self.cost_om = cost_om #Operation & Maintenance cost
+        self.cost_s = cost_s # Salvament cost 
+
 
 class Solar(Generator):
 
-    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s,  c_min, c_max, ef, G_test, R_test):
-        self.ef = ef
-        self.G_test = G_test
-        self.R_test = R_test
+    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, ef, G_test, R_test, n):
+        self.ef = ef #Efficiency 
+        self.G_test = G_test #Rated power of the solar panel in standard test condition
+        self.R_test = R_test #Global irradiance in standard test condition of the PV cell 
+        self.n = n #Number of panels
         self.gen_rule = {}
-        super(Solar, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, c_min, c_max)
+        super(Solar, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s)
     
-    def Solargeneration(self, forecastGt):            
+    def Solargeneration(self, forecastGt): #Gt = irradiance data over the time   
+            #Calculate generation over the time
             for t in list(forecastGt.index.values):
-                self.gen_rule[t] = self.ef * self.G_test * (forecastGt[t]/self.R_test) 
+                self.gen_rule[t] = self.n * self.ef * self.G_test * (forecastGt[t]/self.R_test) 
             return self.gen_rule
 
 
 class Eolic(Generator):
-    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, c_min, c_max, ef, n, s, p, w_in, w_rate, w_out, n_eq):
-        self.ef = ef
-        self.n = n
-        self.s = s
-        self.p = p
-        self.w_in = w_in
-        self.w_rate = w_rate
-        self.w_out = w_out
-        self.n_eq = n_eq
+    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, ef, s_in, s_rate, s_out, rp, n, n_eq):
+        self.ef = ef #Efficiency
+        self.s_in = s_in #Turbine Minimum Generating Speed (Input Speed)
+        self.s_rate = s_rate #Rated speed of the wind turbine
+        self.s_out = s_out # Turbine Maximum Generation Speed (Output Speed)
+        self.rp = rp #Rated power of the wind turbine
+        self.n = n #number of wind turbines
+        self.n_eq = n_eq #n to calculate the generation curve, usually 1,2 or 3
         self.gen_rule = {}
-        super(Eolic, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, c_min, c_max)
+        super(Eolic, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s)
     
-    def Windgeneration(self, forecastWt):
-            t = 0
-            for i in forecastWt:
-                if i <= self.w_in:
+    def Windgeneration(self, forecastWt): #Wt = wind speed over the time
+        #Calculate generation over the time
+            for t in list(forecastWt.index.values):
+                i = forecastWt[t]
+                if i <= self.s_in:
                   self.gen_rule[t] = 0
-                elif i < self.w_rate:
-                  self.gen_rule[t] =  self.c_max*((i**self.n_eq-self.w_in**self.n_eq)/(self.w_rate**self.n_eq-self.w_in**self.n_eq))
-                elif i <= self.w_out:                  
-                  self.gen_rule[t] = self.c_max
+                elif i < self.s_rate:
+                  self.gen_rule[t] =  self.n * self.rp*((i**self.n_eq-self.s_in**self.n_eq)/(self.s_rate**self.n_eq-self.s_in**self.n_eq))
+                elif i <= self.s_out:                  
+                  self.gen_rule[t] = self.n * self.rp
                 else:
                   self.gen_rule[t] = 0
-                t += 1
             return self.gen_rule
 
 
                                
 class Diesel(Generator):
-    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s,  c_min, c_max, ef, G_min, G_max):
-        self.ef = ef
-        self.G_min = G_min
-        self.G_max = G_max
+    def __init__(self, id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, ef, G_min, G_max, n):
+        self.ef = ef #efficiency
+        self.G_min = G_min #Minimun generation to active the Diesel
+        self.G_max = G_max #Rated power, maximum generation
+        self.n = n #Number of diesel generators
         self.gen_rule = {}
-        super(Diesel, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s, c_min, c_max)
+        super(Diesel, self).__init__(id_gen, tec, br, va_op, area, cost_up, cost_r, cost_om, cost_s)
     
-    def Dieselgeneration(self, forecast):
-            t = 0
-            for i in forecast:
-                self.gen_rule[t] = self.c_max
-                t += 1
+    def Dieselgeneration(self, demand): #Calculate maximum generation capacity
+            for t in list(demand.index.values):
+                self.gen_rule[t] = self.n * self.G_max
             return self.gen_rule
 
 
 class Battery():
     def __init__(self, id_bat, tec, br, efc, efd, eb_zero, soc_max, dod_max, alpha, va_op, area, cost_up, cost_r, cost_om, cost_s ):
   
-        self.id_bat = id_bat
-        self.tec = tec
-        self.br = br
-        self.efc = efc
-        self.efd = efd
-        self.eb_zero = eb_zero
-        self.soc_max = soc_max
-        self.dod_max = dod_max
-        self.alpha = alpha
-        self.va_op = va_op
-        self.area = area
-        self.cost_up = cost_up
-        self.cost_r = cost_r
-        self.cost_om = cost_om
-        self.cost_s = cost_s
-        self.soc_min = 0
-
-    def calculatesoc(self):
+        self.id_bat = id_bat #Battery id
+        self.tec = tec #Technology associated to the battery, only allowed: "B"
+        self.br = br #Brand battery
+        self.efc = efc #Charge efficency
+        self.efd = efd #Discharge efficency
+        self.eb_zero = eb_zero #Energy that the battery has stored at time 0
+        self.soc_max = soc_max #Maximum capacity that the battery can storage
+        self.dod_max = dod_max #Maximum depth of discharge
+        self.alpha = alpha #Energy dissipation rate
+        self.va_op = va_op #Operational cost
+        self.area = area #Area
+        self.cost_up = cost_up #Investment cost
+        self.cost_r = cost_r #Replacement cost
+        self.cost_om = cost_om #Operation & Maintenance cost
+        self.cost_s = cost_s #Salvament cost
+        self.soc_min = 0 #Minimum level of energy that must be in the battery
+        #TODO: check if the battery need n
+        
+    def calculatesoc(self): #Calculate soc_min with soc_max and dod_max
         self.soc_min = self.soc_max * (1-self.dod_max)
         return self.soc_min
 
--- src/utilities.py
+++ src/utilities.py
@@ -37,7 +37,7 @@ def read_data(demand_filepath,
     
     return demand_df, forecast_df, generators, batteries, instance_data
 
-def create_objects(generators, batteries, forecast_df):
+def create_objects(generators, batteries, forecast_df, demand_df):
     # Create generators and batteries
     generators_dict = {}
     for k in generators:
@@ -49,18 +49,16 @@ def create_objects(generators, batteries, forecast_df):
         obj_aux.Windgeneration(forecast_df['Wt'])
       elif k['tec'] == 'D':
         obj_aux = Diesel(*k.values())   
-        obj_aux.Dieselgeneration(forecast_df['Wt'])
+        obj_aux.Dieselgeneration(demand_df['demand'])
       generators_dict[k['id_gen']] = obj_aux
       
-    
-    
     batteries_dict = {}
     for l in batteries:
         obj_aux = Battery(*l.values())
         batteries_dict[l['id_bat']] = obj_aux
         batteries_dict[l['id_bat']].calculatesoc()
         
-    # Create technologies list
+    # Create technologies dictionarry
     technologies_dict = dict()
     for bat in batteries_dict.values(): 
       if not (bat.tec in technologies_dict.keys()):
@@ -76,7 +74,6 @@ def create_objects(generators, batteries, forecast_df):
         technologies_dict[gen.tec].add(gen.br)
 
     # Creates renewables dict
-    #another attribute could be created to the class, for the user to determine if it is renewable or not
     renewables_dict = dict()
     for gen in generators_dict.values(): 
         if gen.tec == 'S' or gen.tec == 'W': #or gen.tec = 'H'
@@ -90,13 +87,5 @@ def create_objects(generators, batteries, forecast_df):
 
 
  
-  
-    
-def Calculate_Infraes_cost(generators_opt, batteries_opt):
-     TNPC = sum(generators_opt[k].cost_up for k in generators_opt) + sum(batteries_opt[l].cost_up for l in batteries_opt) 
-     TNPC += sum(generators_opt[k].cost_r for k in generators_opt) + sum(batteries_opt[l].cost_r for l in batteries_opt) 
-     TNPC += sum(generators_opt[k].cost_om for k in generators_opt) + sum(batteries_opt[l].cost_om for l in batteries_opt) 
-     TNPC -= sum(generators_opt[k].cost_s for k in generators_opt) + sum(batteries_opt[l].cost_s for l in batteries_opt) 
-                
-     return TNPC
+
 
