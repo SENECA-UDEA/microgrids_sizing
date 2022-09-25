e468c4416936e3846d793e52fcb5c7f32795f0b6
SebastianCastellanosUdeA <102270541+SebastianCastellanosUdeA@users.noreply.github.com>
2022-09-25 16:37:33 -0500

    erase model by iteration to avoid overwrite

--- src/maindescomposition.py
+++ src/maindescomposition.py
@@ -170,7 +170,7 @@ if (sol_best.results != None):
                                             years = instance_data['years'],
                                             delta = delta)
         #Make model
-        model = opt.make_model_operational(generators_dict = sol_try.generators_dict_sol,
+        model1 = opt.make_model_operational(generators_dict = sol_try.generators_dict_sol,
                                            batteries_dict = sol_try.batteries_dict_sol,  
                                            demand_df=dict(zip(demand_df.t, demand_df.demand)), 
                                            technologies_dict = sol_try.technologies_dict_sol,  
@@ -181,16 +181,19 @@ if (sol_best.results != None):
                                            w_cost = instance_data['w_cost'],
                                            tlpsp = instance_data['tlpsp']) 
         
+        model2 = copy.deepcopy(model1)
+        del model1 
+        
         #Solve the model
-        results, termination = opt.solve_model(model, 
+        results, termination = opt.solve_model(model2, 
                                                optimizer = OPT_SOLVER,
                                                mipgap = MIP_GAP,
                                                tee = TEE_SOLVER)
         
         #Create results
         if termination['Temination Condition'] == 'optimal':
-            sol_try.results.descriptive['LCOE'] = model.LCOE_value.expr()
-            sol_try.results = opt.Results(model)
+            sol_try.results.descriptive['LCOE'] = model2.LCOE_value.expr()
+            sol_try.results = opt.Results(model2)
             sol_try.feasible = True
             sol_current = copy.deepcopy(sol_try)
             #Search the best solution
@@ -202,6 +205,9 @@ if (sol_best.results != None):
             sol_current = copy.deepcopy(sol_try)
     
         sol_current.results.descriptive['area'] = calculate_area(sol_current)
+        del results            
+        del termination
+        del model2      
     
     if ('aux_diesel' in sol_best.generators_dict_sol.keys()):
         print('Not Feasible solutions')
